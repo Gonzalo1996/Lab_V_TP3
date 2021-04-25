1bd9083d81a2dc8e49d4c60eb91ac4d7441e0a65
Albertstone <Albertstone@LAPTOP-10C4F702>
2021-04-25 11:32:51 -0300

    Clase Archivo con funciones de Busqueda y Lectura de Archivo.

--- TP3_GRUPO_4/src/ejercicio1/Archivos.java
+++ TP3_GRUPO_4/src/ejercicio1/Archivos.java
@@ -1,5 +1,53 @@
 package ejercicio1;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Archivos {
 
+	private String ruta;
+	
+	//Función que corrobora que el Archivo exista
+		public boolean Buscar() 
+		{
+			File archivo = new File(ruta);
+			if(archivo.exists())
+				return true;
+			return false;
+		}
+
+	//Función que lee y muestra el Archivo	
+		public void leer() { 
+			
+			FileReader entrada;
+			try {
+				entrada = new FileReader(ruta);
+					//Para poder Leer Ficheros que contengan tildes y ñ
+				BufferedReader miBuffer = new BufferedReader(new InputStreamReader(new FileInputStream(ruta),"cp1252"));
+				String linea = "";
+				while(linea != null) {
+					System.out.println(linea);
+					linea = miBuffer.readLine(); //Devuelve la linea del texto que estoy leyendo
+				}
+				miBuffer.close();
+				entrada.close();
+			}catch (IOException e) {
+				System.out.println("NO SE ENCONTRÓ EL ARCHIVO.");
+			}
+		}
+		
+	//Get y Set
+		public String getRuta() {
+			return ruta;
+		}
+
+		public void setRuta(String ruta) {
+			this.ruta = ruta;
+		}
+	
+	
 }
--- TP3_GRUPO_4/src/ejercicio1/Principal.java
+++ TP3_GRUPO_4/src/ejercicio1/Principal.java
@@ -4,8 +4,25 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Inicio del TP3.");
-
+		System.out.println("TRABAJO PRÁCTICO 3 \n");
+		
+		//Creación Objeto Archivo
+			Archivos file1 = new Archivos();
+			file1.setRuta("Archivos/Personas(1).txt");
+		
+		//Corroboramos que el Archivo exista
+			System.out.println("------------------------------");
+			if(file1.Buscar()) {
+				System.out.println("El Archivo existe");
+			}
+			else {
+				System.out.println("El Archivo no existe");
+			}
+			System.out.println("------------------------------");
+		
+		//Leemos el Archivo Personas(1).txt
+			System.out.println("Lectura Personas(1).txt: ");
+			file1.leer();
 	}
 
 }
